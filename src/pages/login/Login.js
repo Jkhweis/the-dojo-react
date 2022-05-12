a3d87ba45d8b9e08c0e1a398f60764fa585e156a
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-05-12 13:21:34 -0500

    Logging Users In

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
+
 //styles
 import './Login.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
