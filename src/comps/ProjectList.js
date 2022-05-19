b3d8cbdb7516ef5308fa47041c4dae4aadce41f5
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-05-18 23:29:52 -0500

    Fetching & Displaying Projects

---
+++ src/comps/ProjectList.js
@@ -0,0 +1,14 @@
+//styles
+import './ProjectList.css';
+
+export default function ProjectList({ projects }) {
+  return (
+    <div>
+      {projects.length === 0 && <p>No projects yet</p>}
+
+      {projects.map((project) => (
+        <div key={project.id}>{project.name}</div>
+      ))}
+    </div>
+  );
+}
